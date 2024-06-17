58b324a8eee789565bd0a796d4bf8243758e3fcb
Ahmad sabili A <ahmadsabili0081@gmail.com>
2024-06-18 00:40:37 +0700

    test commit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./pages/App";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route exact path="/" >
-          <App />
-        </Route>
-        <Route exact path="/:id" >
-          <App />
-        </Route>
+        <Route path="/" element={<App />} />
+        <Route path="/:id" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
